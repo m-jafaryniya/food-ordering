901c597917354bdec90c3a483431406f74251bbb
m-jafaryniya <mjafaryniya85@gmail.com>
2026-06-07 14:50:48 +0330

    Initial commit

---
+++ include/models/User.h
@@ -0,0 +1,15 @@
+//
+// Created by hp on 03/06/2026.
+//
+
+#ifndef FOOD_ORDERING_SYSTEM_USER_H
+#define FOOD_ORDERING_SYSTEM_USER_H
+
+class User {
+public:
+
+private:
+
+}
+
+#endif //FOOD_ORDERING_SYSTEM_USER_H
