a5df52734afcbbc27262eb0b6c7c40a34ad20725
m-jafaryniya <mjafaryniya85@gmail.com>
2026-06-11 15:40:26 +0330

    Add Customer.h

---
+++ include/models/Customer.h
@@ -0,0 +1,17 @@
+//
+// Created by hp on 09/06/2026.
+//
+
+#ifndef FOOD_ORDERING_SYSTEM_CUSTOMER_H
+#define FOOD_ORDERING_SYSTEM_CUSTOMER_H
+#include "User.h"
+
+class Customer : public User{
+    public:
+    void Customer();
+
+    private:
+
+}
+
+#endif //FOOD_ORDERING_SYSTEM_CUSTOMER_H
