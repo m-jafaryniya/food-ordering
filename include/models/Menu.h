de9ca3bff28e6025af1771b7c07f069f5a5bcb12
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-15 20:46:07 +0330

    Update Address model

--- include/models/Menu.h
+++ include/models/Menu.h
@@ -9,6 +9,16 @@ class Menu {
     Menu(const std::string &ItemName, const std::string &ItemDescription, double basePrice);
     virtual ~Menu();
     virtual std::string get_ItemType()=0;
+    void set_ItemId(int id);
+    void set_ItemName(const std::string& name);
+    void set_ItemDescription(const std::string& description);
+    void set_BasePrice(double price);
+    void set_available(bool available);
+    int get_ItemId() const;
+    std::string get_ItemName() const;
+    std::string get_ItemDescription() const;
+    double get_BasePrice() const;
+    bool get_available() const;
 
     private:
     int ItemId;
--- src/models/Menu.cpp
+++ src/models/Menu.cpp
@@ -12,3 +12,43 @@ Menu::Menu(const std::string &ItemName, const std::string &ItemDescription, cons
 }
 
 Menu::~Menu() {}
+
+void Menu::set_ItemId(const int id) {
+    this->ItemId = id;
+}
+
+void Menu::set_ItemName(const std::string &name) {
+    this->ItemName = name;
+}
+
+void Menu::set_ItemDescription(const std::string &description) {
+    this->ItemDescription = description;
+}
+
+void Menu::set_BasePrice(const double price) {
+    basePrice = price;
+}
+
+void Menu::set_available(bool available) {
+    this->available = available;
+}
+
+int Menu::get_ItemId() const {
+    return ItemId;
+}
+
+std::string Menu::get_ItemName() const {
+    return ItemName;
+}
+
+std::string Menu::get_ItemDescription() const {
+    return ItemDescription;
+}
+
+double Menu::get_BasePrice() const {
+    return basePrice;
+}
+
+bool Menu::get_available() const {
+    return available;
+}
