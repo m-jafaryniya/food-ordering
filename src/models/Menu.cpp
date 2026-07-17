3c964d966cb63bc5ad9706f0d14be090e0bd768a
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-17 11:56:47 +0330

    Change some notations in Menu model

--- src/models/Menu.cpp
+++ src/models/Menu.cpp
@@ -5,7 +5,7 @@
 Menu::Menu() {}
 
 Menu::Menu(const std::string &ItemName, const std::string &ItemDescription, const double basePrice) {
-    this->ItemId = IdMaker::get_MenuID();
+    this->ItemId = IdMaker::getMenuID();
     this->ItemName = ItemName;
     this->ItemDescription = ItemDescription;
     this->basePrice = basePrice;
