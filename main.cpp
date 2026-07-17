26f03203259df8e36ce5a32c12e6a0a669bc86ea
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-17 17:08:06 +0330

    Update main

--- main.cpp
+++ main.cpp
@@ -8,18 +8,29 @@
 #include "services/CustomerService.h"
 #include "services/RestaurateurService.h"
 #include "services/Login.h"
+#include "services/SystemAdministratorService.h"
 
 int main() {
-    Database db("food_ordering_system");
-    db.createTables();
+    Database db("food_ordering_system.db");
+    Login loginManager(&db);
 
-    CustomerDAO customerDAO(&db);
-    RestaurantDAO restaurantDAO(&db);
-    RestaurateurDAO restaurateurDAO(&db);
-    SystemAdministratorDAO system_administratorDAO(&db);
+    User* loggedInUser = loginManager.login();
+    if (loggedInUser == nullptr) {
+        std::cout << "Login Failed" << std::endl;
+        return 1;
+    }
+    std::cout << "Welcome" << std::endl;
 
-    CustomerService (customerDAO, restaurantDAO);
-    RestaurateurService (customerDAO, restaurantDAO);
-    Login (customerDAO, restaurateurDAO, system_administratorDAO);
+    if (Customer* customer = dynamic_cast<Customer *>(loggedInUser)){
+        CustomerService customerService(&db);
+    }
+    else if (Restaurateur* restaurateur = dynamic_cast<Restaurateur *>(loggedInUser)){
+            RestaurateurService restaurateurService(&db);
+    }
+    else if (SystemAdministrator* administrator = dynamic_cast<SystemAdministrator *>(loggedInUser)){
+            SystemAdministratorService systemAdministratorService(&db);
+    }
 
+    delete loggedInUser;
+    return 0;
 }
