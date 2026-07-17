ab2b85448dac7cdb043ff89ac1ae4b5b2f0f1928
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-17 17:01:42 +0330

    Update System Administrator source

--- src/models/SystemAdministrator.cpp
+++ src/models/SystemAdministrator.cpp
@@ -1,6 +1,8 @@
 
 #include "models/SystemAdministrator.h"
 
+#include "dao/Database.h"
+
 SystemAdministrator::SystemAdministrator() {}
 
 SystemAdministrator::SystemAdministrator(const std::string &phoneNumber, const std::string &password, const std::string &userName)
