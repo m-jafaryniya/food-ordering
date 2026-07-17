0f01f0b1b7d204a34d40c9eda0b66f413d4f5a08
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-17 11:18:47 +0330

    Update Customer model

--- include/models/Customer.h
+++ include/models/Customer.h
@@ -7,12 +7,12 @@ class Customer : public User{
     public:
     Customer();
     Customer(const std::string &phoneNumber, const std::string &password, const std::string &userName);
-    virtual ~Customer();
-    void set_wallet(double wallet);
-    double get_wallet() const;
+    ~Customer() override;
+    void setWallet(double wallet);
+    double getWallet() const;
 
     private:
-    double wallet;
+    double wallet = 0.0;
 
 };
 
--- src/models/Customer.cpp
+++ src/models/Customer.cpp
@@ -1,19 +1,19 @@
 
 #include "models/Customer.h"
-#include "models/User.h"
 
 Customer::Customer() {}
 
 Customer::Customer(const std::string &phoneNumber, const std::string &password, const std::string &userName)
-:User(phoneNumber, password, userName){}
+:User(phoneNumber, password, userName) {
+}
 
-Customer::~Customer() {}
+Customer::~Customer() = default;
 
-void Customer::set_wallet(double wallet) {
+void Customer::setWallet(double wallet) {
     this->wallet = wallet;
 }
 
-double Customer::get_wallet() const {
+double Customer::getWallet() const {
     return wallet;
 }
 
