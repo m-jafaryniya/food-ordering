a7c9f60e4c090921bebf2e5e19a2fdea9d8c583a
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-17 09:47:28 +0330

    Update System administrator service

--- include/services/SystemAdministratorService.h
+++ include/services/SystemAdministratorService.h
@@ -1,11 +1,33 @@
 
-
 #ifndef FOOD_ORDERING_SYSTEM_SYSTEMADMINISTRATORSERVICE_H
 #define FOOD_ORDERING_SYSTEM_SYSTEMADMINISTRATORSERVICE_H
+
+#include "dao/CustomerDAO.h"
+#include "dao/RestaurateurDAO.h"
+#include "dao/OrderDAO.h"
+#include "dao/RestaurantDAO.h"
 
 class SystemAdministratorService {
     public:
     SystemAdministratorService();
+    bool addRestaurant(const Restaurant &restaurant);
+    bool activeRestaurant(int restaurantId);
+    bool deActiveRestaurant(int restaurantId);
+    std::vector<Restaurant> getRestaurants();
+
+    int getRestaurantCount();
+    int getRestaurateurCount();
+    int getCustomerCount();
+    int getOrderCount(int restaurantId);
+    double getRestaurantTotalSales(int restaurantId);
+    double getTotalSales();
+    void systemReport();
+
+private:
+    RestaurantDAO restaurantDAO;
+    CustomerDAO customerDAO;
+    RestaurateurDAO restaurateurDAO;
+    OrderDAO orderDAO;
 };
 
 #endif //FOOD_ORDERING_SYSTEM_SYSTEMADMINISTRATORSERVICE_H
--- src/services/SystemAdministratorService.cpp
+++ src/services/SystemAdministratorService.cpp
@@ -1,2 +1,66 @@
 
 #include "services/SystemAdministratorService.h"
+
+SystemAdministratorService::SystemAdministratorService() {
+
+}
+
+bool SystemAdministratorService::addRestaurant(const Restaurant &restaurant) {
+
+}
+
+bool SystemAdministratorService::activeRestaurant(int restaurantId) {
+    Restaurant restaurant;
+    restaurantDAO.getRestaurantById(restaurantId);
+    restaurant.set_active(true);
+    restaurantDAO.updateRestaurant(restaurant);
+    return true;
+}
+
+bool SystemAdministratorService::deActiveRestaurant(int restaurantId) {
+    Restaurant restaurant;
+    restaurantDAO.getRestaurantById(restaurantId);
+    restaurant.set_active(false);
+    restaurantDAO.updateRestaurant(restaurant);
+    return true;
+}
+
+std::vector<Restaurant> SystemAdministratorService::getRestaurants() {
+    return restaurantDAO.getRestaurants();
+}
+
+int SystemAdministratorService::getRestaurantCount() {
+    return restaurantDAO.getRestaurants().size();
+}
+
+int SystemAdministratorService::getRestaurateurCount() {
+    return restaurateurDAO.getRestaurateurs().size();
+}
+
+int SystemAdministratorService::getCustomerCount() {
+    return customerDAO.getCustomers().size();
+}
+
+int SystemAdministratorService::getOrderCount(int restaurantId) {
+    return orderDAO.getOrders().size();
+}
+
+double SystemAdministratorService::getRestaurantTotalSales(int restaurantId) {
+    double total = 0;
+    std::vector<Order> orders = orderDAO.getRestaurantOrders(restaurantId);
+    for (const Order &order : orders) {
+        if (order.get_status() == OrderStatus::delivered) {
+            total += order.get_totalPrice();
+        }
+    }
+    return total;
+}
+
+double SystemAdministratorService::getTotalSales() {
+    double total =0 ;
+    std::vector<Order> orders = orderDAO.getOrders();
+    for (const Order &order : orders) {
+        total+=order.get_totalPrice();
+    }
+    return total;
+}
