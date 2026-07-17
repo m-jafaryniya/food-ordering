65e8b63599eaa041952229b7bd2e8e62f0d2c63e
m-jafaryniya <mjafaryniya85@gmail.com>
2026-07-17 09:44:59 +0330

    Update Restaurateur service

--- include/services/RestaurateurService.h
+++ include/services/RestaurateurService.h
@@ -1,10 +1,42 @@
 
 #ifndef FOOD_ORDERING_SYSTEM_RESTAURATEURSERVICE_H
 #define FOOD_ORDERING_SYSTEM_RESTAURATEURSERVICE_H
+#include "dao/MenuDAO.h"
+#include "dao/OrderDAO.h"
+#include "dao/RestaurantDAO.h"
+#include "models/Restaurateur.h"
+#include "models/Restaurant.h"
+#include "services/Order.h"
 
 class RestaurateurService {
 public:
     RestaurateurService();
+    bool insertRestaurant (const Restaurateur& owner, const std::string name);
+    bool editRestaurant (const Restaurant& restaurant);
+    bool deleteRestaurant (int restaurantId);
+    bool changeRestaurantStatus (int restaurantId, bool isOpen);
+    Restaurant getRestaurant (int restaurantId);
+    std::vector<Restaurant> getRestaurants(int ownerId);
+
+    bool addMenuItem(const Menu& item);
+    bool editMenuItem(const Menu& item);
+    bool deleteMenuItem(const Menu& item);
+    bool changeMenuItemPrice(int itemId, double newPrice);
+    bool changeMenuItemAvailability(int itemId, bool available);
+    std::vector<Menu> getMenuItems(int restaurantId);
+
+    std::vector<Order> getRestaurantOrders(int restaurantId);
+    std::vector<Order> getPendingOrders(int restaurantId);
+    bool acceptOrder(int orderId);
+    bool cancelOrder(int orderId);
+    bool readyOrder(int orderId);
+    bool deliveredOrder(int orderId);
+    bool changeOrderStatus(int orderId, OrderStatus status);
+
+private:
+    RestaurantDAO restaurantDAO;
+    MenuDAO menuDAO;
+    OrderDAO orderDAO;
 };
 
 #endif //FOOD_ORDERING_SYSTEM_RESTAURATEURSERVICE_H
--- src/services/RestaurateurService.cpp
+++ src/services/RestaurateurService.cpp
@@ -1,2 +1,103 @@
 
 #include "services/RestaurateurService.h"
+
+RestaurateurService::RestaurateurService() {
+
+}
+
+bool RestaurateurService::insertRestaurant(const Restaurateur &owner, const std::string name) {
+    Restaurant restaurant;
+    restaurant.set_ownerId(owner.get_id());
+    restaurant.set_name(name);
+
+    restaurantDAO.insertRestaurant(restaurant);
+    return true;
+}
+
+bool RestaurateurService::editRestaurant(const Restaurant &restaurant) {
+    restaurantDAO.updateRestaurant(restaurant);
+    return true;
+}
+
+bool RestaurateurService::deleteRestaurant(int restaurantId) {
+    menuDAO.deleteDrink();
+    menuDAO.deleteFood();
+    restaurantDAO.deleteRestaurant(restaurantId);
+    return true;
+}
+
+bool RestaurateurService::changeRestaurantStatus(int restaurantId, bool isOpen) {
+    restaurantDAO.changeRestaurantStatus(restaurantId, isOpen);
+    return true;
+}
+
+Restaurant RestaurateurService::getRestaurant(int restaurantId) {
+    return restaurantDAO.getRestaurantById(restaurantId);
+}
+
+std::vector<Restaurant> RestaurateurService::getRestaurants(int ownerId) {
+    return restaurantDAO.getRestaurantsByOwner();
+}
+
+bool RestaurateurService::addMenuItem(const Menu &item) {
+    menuDAO.addFood();
+    menuDAO.addDrink();
+    return true;
+}
+
+bool RestaurateurService::editMenuItem(const Menu &item) {
+    menuDAO.updateDrink();
+    menuDAO.updateFood();
+    return true;
+}
+
+bool RestaurateurService::deleteMenuItem(const Menu &item) {
+    menuDAO.deleteDrink();
+    menuDAO.deleteFood();
+    return true;
+}
+
+bool RestaurateurService::changeMenuItemPrice(int itemId, double newPrice) {
+    menuDAO.changePrice(itemId, newPrice);
+    return true;
+}
+
+bool RestaurateurService::changeMenuItemAvailability(int itemId, bool available) {
+    menuDAO.changeItemAvailability(itemId, available);
+    return true;
+}
+
+std::vector<Order> RestaurateurService::getRestaurantOrders(int restaurantId) {
+    return orderDAO.getRestaurantOrders(restaurantId);
+}
+
+std::vector<Order> RestaurateurService::getPendingOrders(int restaurantId) {
+    return orderDAO.getOrderByStatus(restaurantId, OrderStatus::pending);
+}
+
+bool RestaurateurService::changeOrderStatus(int orderId, OrderStatus status) {
+    orderDAO.updateOrderStatus(orderId, status);
+    return true;
+}
+
+bool RestaurateurService::acceptOrder(int orderId) {
+    changeOrderStatus(orderId, OrderStatus::preparing);
+    return true;
+}
+
+bool RestaurateurService::cancelOrder(int orderId) {
+    changeOrderStatus(orderId, OrderStatus::cancelled);
+    return true;
+}
+
+bool RestaurateurService::readyOrder(int orderId) {
+    changeOrderStatus(orderId, OrderStatus::ready);
+    return true;
+}
+
+bool RestaurateurService::deliveredOrder(int orderId) {
+    changeOrderStatus(orderId, OrderStatus::delivered);
+    return true;
+}
+
+
